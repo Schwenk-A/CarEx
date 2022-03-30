704f34fbaf05b9f32b225da753cc13aeb455ec21
Andrea Schwenk <andreaschwenk3@gmail.com>
2022-03-30 10:04:11 -0500

    Completed assignment

---
+++ BasicClasses/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicClasses
+{
+    internal class Car
+    {
+     public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+
+
+
+    }
+
+    
+
+}
+
---
+++ BasicClasses/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BasicClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car newVehicle = new Car();
+
+            newVehicle.Make = "Dodge";
+            newVehicle.Model = "Grand Caravan";
+            newVehicle.Year = 1996;
+
+
+
+
+            Console.WriteLine($"My new car is a {newVehicle.Year}{newVehicle.Make} {newVehicle.Model} ");
+        }
+    }
+}
